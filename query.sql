fcf62a201f1343cf9bc32545c60be65a4e734796
Arnob Tanvir <122850109+tanvir0188@users.noreply.github.com>
2026-06-15 18:45:56 +0600

    query 6 and 7 done

--- query.sql
+++ query.sql
@@ -103,3 +103,16 @@ select u.user_id, u.full_name, b.booking_id
 from users u
 left join bookings b
 on u.user_id=b.user_id;
+
+--query 6:
+select booking_id, match_id, total_cost from bookings
+where total_cost <(
+  select avg(total_cost)
+  from bookings
+);
+
+--query 7:
+select match_id, fixture, base_ticket_price from matches
+order by base_ticket_price desc
+limit 2
+offset 1;
